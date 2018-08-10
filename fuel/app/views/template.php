dbbc58466273eaf00907b47a681dcaec450ba1d4
Javi Carretero <rentonr11@gmail.com>
2018-08-10 23:12:21 +0200

    Changing the name of the company

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -29,8 +29,8 @@ else{
 	<div class="container">
 		<div class="col-md-12 cabecera">
 			<!--<h1><?php /*echo $title;*/ ?></h1>-->
-            <a alt="Ir al menú principal" href="http://gestion.aceitunascoria.es/fuel/public/" class="no-decoration"><?php echo Asset::img('aceitunas.jpg',array("class"=>"logo","alt"=>"Ir al menú principal")); ?>
-            <h1>Aceitunas Coria S.L.</h1></a>
+            <a alt="Ir al menú principal" href="http://prodi.aceitunascoria.es/fuel/public/" class="no-decoration"><?php echo Asset::img('aceitunas.jpg',array("class"=>"logo","alt"=>"Ir al menú principal")); ?>
+            <h1>Prodiquivir S.L.</h1></a>
 			<hr>
 <?php if (Session::get_flash('success')): ?>
 			<div class="alert alert-success">
